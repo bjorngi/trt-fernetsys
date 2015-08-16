abc399c2e2df91b44c70f07b299f035408ed70b2
Dag Jomar Mersland <dagjomar@gmail.com>
2015-08-16 21:33:18 +0200

    Working essence styles

--- app/main.jsx
+++ app/main.jsx
@@ -1,31 +1,90 @@
-import React from "react"
-import { Btn, BtnItem, Icon, Block, Text } from 'react-essence';
+'use strict';
+var Essence = require("react-essence");
+import "./css/essence.css";
 
-class MainWindow extends React.Component {
-  render() {
-    return (
-      <Block type='div' classes={'brick brick-6'}>
+var body = document.body, // default React render path is html body
+    byID = document.getElementById("header"),
+    header = document.querySelector(".header"),
+    content = document.querySelector(".content"),
+    footer = document.querySelector(".footer"),
+    React = Essence.React,
+    AppBar = Essence.AppBar,
+    BottomSheets = Essence.BottomSheets,
+    BottomSheetsItem = Essence.BottomSheetsItem,
+    Btn = Essence.Btn,
+    BtnItem = Essence.BtnItem,
+    Block = Essence.Block,
+    Card = Essence.Card,
+    CardItem = Essence.CardItem,
+    CardItemHeader = Essence.CardItemHeader,
+    CardItemContent = Essence.CardItemContent,
+    CardItemFooter = Essence.CardItemFooter,
+    Chip = Essence.Chip,
+    ChipItem = Essence.ChipItem,
+    DatePicker = Essence.DatePicker,
+    DatePickerItem = Essence.DatePickerItem,
+    DatePickerHeader = Essence.DatePickerHeader,
+    DatePickerHeaderDay = Essence.DatePickerHeaderDay,
+    DatePickerHeaderDate = Essence.DatePickerHeaderDate,
+    DatePickerContent = Essence.DatePickerContent,
+    DatePickerFooter = Essence.DatePickerFooter,
+    Dialog = Essence.Dialog,
+    DialogItem = Essence.DialogItem,
+    DialogItemHeader = Essence.DialogItemHeader,
+    DialogItemContent = Essence.DialogItemContent,
+    DialogItemFooter = Essence.DialogItemFooter,
+    Divider = Essence.Divider,
+    Icon = Essence.Icon,
+    Image = Essence.Image,
+    Input = Essence.Input,
+    InputItem = Essence.InputItem,
+    List = Essence.List,
+    ListItem = Essence.ListItem,
+    Menu = Essence.Menu,
+    MenuItem = Essence.MenuItem,
+    Navigation = Essence.Navigation,
+    Paper = Essence.Paper,
+    PaperItem = Essence.PaperItem,
+    Progress = Essence.Progress,
+    Slider = Essence.Slider,
+    SliderItem = Essence.SliderItem,
+    Snackbar = Essence.Snackbar,
+    SnackbarItem = Essence.SnackbarItem,
+    Switch = Essence.Switch,
+    SwitchItem = Essence.SwitchItem,
+    TabItem = Essence.TabItem,
+    TabMenu = Essence.TabMenu,
+    Text = Essence.Text,
+    Toast = Essence.Toast,
+    ToastItem = Essence.ToastItem,
+    ToolBar = Essence.ToolBar,
+    BackgroundColor = Essence.BackgroundColor,
+    ClassNames = Essence.ClassNames,
+    ClickPosition = Essence.ClickPosition,
+    DateFormat = Essence.DateFormat,
+    Mobile = Essence.Mobile,
+    Position = Essence.Position,
+    PositionHorizontal = Essence.PositionHorizontal,
+    PubSub = Essence.PubSub;
 
-        <Text type='h2' classes={"e-display-1 e-text-blue-700"}>Some title</Text>
-        <Block type='div' classes={'e-text-grey-400 e-caption'}>
-          <Text>Some text</Text>
-        </Block>
 
-        <Block type='div' classes={'brick brick-6 e-text-right e-padding-top-15'}>
-          <BtnItem
-            classes={'raised e-background-pink-A200'}
-            onClick={this.yourActionOnClick}
-            label='Button 1'/>
+React.render(
+    <AppBar>
+      include any type of components like: Menu, Btn, etc
+    </AppBar>
+    ,header
+);
 
-          <BtnItem
-            classes={'raised e-background-pink-A200'}
-            onClick={this.yourActionOnClick}
-            label='Button 2'/>
-        </Block>
-      </Block>
+React.render(
+    <Block type='div' classes='e-background-yellow-500 e-text-center'>
+      your content by class .content here
+    </Block>
+    ,content
+);
 
-        )
-  }
-}
-
-React.render(<MainWindow/>, document.getElementById('wrapper'));
+React.render(
+    <Block type='div' classes='e-background-blue-500 e-text-center'>
+      your content by class .footer here
+    </Block>
+    ,footer
+);
