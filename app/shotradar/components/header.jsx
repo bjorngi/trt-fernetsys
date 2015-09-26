376098ef1ebf0a77147f5f6264d4d8d7575783c2
Dag Jomar Mersland <dagjomar@gmail.com>
2015-09-26 18:48:06 +0200

    header styling

--- app/shotradar/components/header.jsx
+++ app/shotradar/components/header.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
 import { AppBar, Menu, MenuItem, Block, Btn, BtnItem, Icon, Text } from 'react-essence';
 
+import './header.css';
+
 var HeaderBar = React.createClass({
   render() {
     return (
@@ -11,76 +13,21 @@ var HeaderBar = React.createClass({
           icon='navigation-menu'
           >
           <MenuItem>
-            <Text>New project</Text>
+            <Text>Overview</Text>
           </MenuItem>
           <MenuItem>
-            <Text>New user</Text>
+            <Text>Another view</Text>
           </MenuItem>
           <MenuItem>
-            <Text>More tools</Text>
+            <Text>Another view</Text>
           </MenuItem>
           <MenuItem>
-            <Text>Exit</Text>
+            <Text>Another view</Text>
           </MenuItem>
         </Menu>
 
-        <Menu
-          id='menu-appbar-left'
-          classes={'e-text-color-blue-500 cover e-left'}
-          placeholder='Options'
-          >
-          <MenuItem>
-            <Icon name='notification-sms' />
-            <Text>Notifications</Text>
-          </MenuItem>
-          <MenuItem>
-            <Icon name='notification-tap-and-play' />
-            <Text>Tap & Pay</Text>
-          </MenuItem>
-          <Block type='li' classes={'divider'} />
-          <MenuItem>
-            <Text type='a' href='http://www.pearlhq.com'>
-              Pearl
-            </Text>
-          </MenuItem>
-          <MenuItem>
-            <Text type='a' href='http://www.google.com'>
-              Google
-            </Text>
-          </MenuItem>
-        </Menu>
+        <h4 className={'sr-header-title'} >Overview</h4>
 
-        <Menu
-          id='menu-appbar-right'
-          classes={'e-text-color-blue-500 cover e-right'}
-          icon='navigation-more-vert'
-          right={true}
-          >
-          <MenuItem>
-            <Text>Profile</Text>
-          </MenuItem>
-          <MenuItem>
-            <Text>Settings</Text>
-          </MenuItem>
-          <Block type='li' classes={'divider'} />
-          <MenuItem>
-            <Text>Support</Text>
-          </MenuItem>
-          <MenuItem>
-            <Text>Logout</Text>
-          </MenuItem>
-        </Menu>
-
-        <Btn>
-          <BtnItem
-            icon='action-favorite'
-            classes={'simple-button e-right'}
-          />
-          <BtnItem
-            icon='action-search'
-            classes={'simple-button e-right'}
-          />
-        </Btn>
       </AppBar>
     );
   }
